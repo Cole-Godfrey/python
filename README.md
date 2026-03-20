661b4c0fd473687051474939447650ee9f27d4c8
Cole Godfrey <coleg@Coles-MacBook-Pro.local>
2026-03-19 18:26:25 -0700

    Learned string methods.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This is a repository which contains all of my code that I wrote when learning Py
 Here is what I have learned so far:
 - Variables
 - Multiple assignment
+- String methods
---
+++ stringMethods.py
@@ -0,0 +1,12 @@
+name = "Cole"
+
+print(len(name))
+print(name.find("l"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("o", "a"))
+print(name * 3)
